6035d25adfd9d7302a4cfabdf4f6e73b7444e1d5
AbdalrhmanAmmar <bodyty2n@gmail.com>
2025-10-31 19:25:34 +0200

    react-app

--- src/main.tsx
+++ src/main.tsx
@@ -4,3 +4,18 @@ import './index.css'
 import './i18n/index.ts'
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log("Service Worker registered:", registration);
+      })
+      .catch((error) => {
+        console.log("Service Worker registration failed:", error);
+      });
+  });
+}
+
